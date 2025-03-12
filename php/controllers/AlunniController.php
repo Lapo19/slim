d13c704f82abae9e2d15f833a2a0ad06694acdcd
lai2-24 <lai2-24@itismeucci.com>
2025-03-12 10:33:09 +0100

    1

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -12,4 +12,57 @@ class AlunniController
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
+
+  public function view(Request $request, Response $response, $args){
+    $id = $args['id'];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id = $id");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function create(Request $request, Response $response, $args){
+    $body = json_decode($request->getBody()->getContents(), true);
+    $nome = $body['nome'];
+    $cognome = $body['cognome'];
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("INSERT INTO alunni(nome,cognome) VALUES ('$nome','$cognome') ");
+    if($result && $mysqli_connection->affected_rows > 0){
+      $response->getBody()->write(json_encode(array("message"=>"Success")));
+    } else {
+      $response->getBody()->write(json_encode(array("message"=>$mysqli_connection->error)));
+    }
+    return $response->withHeader("Content-type", "application/json")->withStatus(201);
+  }
+
+  public function update(Request $request, Response $response, $args){
+    sleep(1);
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $id = $args['id'];
+    $body = json_decode($request->getBody()->getContents(), true);
+    $nome = $body['nome'];
+    $cognome = $body['cognome'];
+    $result = $mysqli_connection->query("UPDATE alunni SET nome = '$nome',cognome = '$cognome' WHERE id = '$id'");
+    if($result && $mysqli_connection->affected_rows > 0){
+      $response->getBody()->write(json_encode(array("message"=>"Success")));
+    } else {
+      $response->getBody()->write(json_encode(array("message"=>$mysqli_connection->error)));
+    }
+    return $response->withHeader("Content-type", "application/json")->withStatus(201);
+  }
+
+  public function delete(Request $request, Response $response, $args){
+    sleep(1);
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $id = $args['id'];
+    $result = $mysqli_connection->query("DELETE FROM alunni WHERE id= '$id'");
+    if($result && $mysqli_connection->affected_rows > 0){
+      $response->getBody()->write(json_encode(array("message"=>"Success")));
+    } else {
+      $response->getBody()->write(json_encode(array("message"=>$mysqli_connection->error)));
+    }
+    return $response->withHeader("Content-type", "application/json")->withStatus(201);
+  }
 }
--- php/index.php
+++ php/index.php
@@ -1,11 +1,28 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/controllers/HelloController.php';
 require __DIR__ . '/controllers/AlunniController.php';
 
 $app = AppFactory::create();
 
-$app->get('/alunni', "AlunniController:index");
+$app->get('/hello', "HelloController:hello");
+$app->get('/hello/{name}', "HelloController:helloName");
+$app->get('/json/{name}', "HelloController:json_name");// ritornare un json
 
+$app->get('/alunni', "AlunniController:index");
+$app->get('/alunni/{id}', "AlunniController:view");
+$app->post('/alunni', "AlunniController:create");
+$app->put('/alunni/{id}', "AlunniController:update");
+$app->delete('/alunni/{id}', "AlunniController:delete");
+
+/*curl localhost:8080/alunni
+curl localhost:8080/alunni/2
+curl -X POST localhost:8080/alunni --data '{"nome":"aaa","cognome":"bbb"}' -H "Content-Type', 'application/json"
+curl -X PUT localhost:8080/alunni/2 --data '{"nome":"aaa","cognome":"bbb"}' -H "Content-Type', 'application/json"
+curl -X DELETE localhost:8080/alunni/3
+*/
 $app->run();
